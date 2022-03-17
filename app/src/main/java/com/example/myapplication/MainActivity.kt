3e1fb2c29be9d02b1fa34963cde48fb5bfa79e82
DESKTOP-B6N5TV2\nhan <devildanger976@gmail.com>
2022-03-17 10:29:19 +0700

    change to grid recyclerview

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mHeros:MutableList<Hero>
@@ -14,17 +16,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         mRecyclerHero=findViewById(R.id.recyclerHero)
         mHeros= mutableListOf<Hero>()
+        val gridLayoutManager = GridLayoutManager(applicationContext, 3)
+
+        creatHeroList()
         mHeroAdapter= HeroAdapter(this,mHeros)
         mRecyclerHero.adapter=mHeroAdapter
-        mRecyclerHero.layoutManager=LinearLayoutManager(this)
+        //mRecyclerHero.layoutManager=LinearLayoutManager(this)
+        mRecyclerHero.layoutManager=gridLayoutManager
     }
+
     private fun creatHeroList()
     {
-        mHeros.add(Hero("thor", com.google.android.material.R.drawable.abc_ab_share_pack_mtrl_alpha))
-        mHeros.add(Hero("ironmen", com.google.android.material.R.drawable.avd_hide_password))
-        mHeros.add(Hero("hulk", com.google.android.material.R.drawable.design_ic_visibility))
-        mHeros.add(Hero("spidermen", com.google.android.material.R.drawable.abc_ic_menu_selectall_mtrl_alpha))
-        mHeros.add(Hero("sad", com.google.android.material.R.drawable.notification_template_icon_low_bg))
+        mHeros.add(Hero("thor", R.drawable.thor))
+        mHeros.add(Hero("ironmen", R.drawable.dafodil))
+        mHeros.add(Hero("hulk", R.drawable.hulk))
+        mHeros.add(Hero("spidermen", R.drawable.spiderman))
+
+        //mHeros.add(Hero("sad", com.google.android.material.R.drawable.notification_template_icon_low_bg))
 
     }
 }
