c959838afff63f546272f37a31008f39469930f4
DESKTOP-B6N5TV2\nhan <devildanger976@gmail.com>
2022-03-25 18:26:11 +0700

    progressBar Create a Notification

--- app/src/main/java/com/example/myapplication/ImageEditorActivity.kt
+++ app/src/main/java/com/example/myapplication/ImageEditorActivity.kt
@@ -1,107 +1,190 @@
 package com.example.myapplication
 
 import android.app.Activity
+import android.app.Notification.EXTRA_NOTIFICATION_ID
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import android.database.Cursor
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.os.Environment
+import android.os.*
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.*
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.OutputStream
 
 
+/**
+ *
+ */
 class ImageEditorActivity : AppCompatActivity() {
-    private lateinit var mHeros:MutableList<Hero>
-    private lateinit var  mRecyclerList: RecyclerView
+    private lateinit var mHeros: MutableList<Hero>
+    private lateinit var mRecyclerList: RecyclerView
     private lateinit var mHeroAdapter: HeroAdapter
-    lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private val id: String = "my_channel_01"
+
+    /**
+     *
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image)
         val filePath = intent.getStringExtra("FILE_PATH")
         val mPhotograp: ImageView = findViewById(R.id.image_view)
-        val uri: Uri
-        uri = Uri.parse(filePath)
+        val uri: Uri = Uri.parse(filePath)
         mPhotograp.setImageURI(uri)
-        val save:Button=findViewById(R.id.save)
-        val close: FloatingActionButton =findViewById(R.id.close)
-        close.setOnClickListener(){
+        val save: Button = findViewById(R.id.save)
+        val close: Button = findViewById(R.id.close)
+        close.setOnClickListener() {
             finish()
         }
-        val share:FloatingActionButton=findViewById(R.id.share)
-        share.setOnClickListener(){
-            val bmpUri = getLocalBitmapUri(mPhotograp)
-            if (bmpUri != null) {
-                // Construct a ShareIntent with link to image
-                val shareIntent = Intent()
-                shareIntent.action = Intent.ACTION_SEND
-                shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri)
-                shareIntent.type = "image/png"
-                shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                // Launch sharing dialog for image
-                startActivity(Intent.createChooser(shareIntent, "Share Image"))
-            } else {
-                // ...sharing failed, handle error
-            }
+        val share: Button = findViewById(R.id.share)
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
+        progressBar.visibility = View.GONE
+        share.setOnClickListener() {
+            progressBar.visibility = View.VISIBLE
+            var i = progressBar.progress
+            Thread(Runnable {
+                // this loop will run until the value of i becomes 99
+                while (i < 100) {
+                    i += 1
+                    // Update the progress bar and display the current value
+                    val handler: Handler = Handler()
+                    handler.post(Runnable {
+                        progressBar.progress = i
+                    })
+                    try {
+                        Thread.sleep(100)
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+                }
+                val bmpUri = getLocalBitmapUri(mPhotograp)
+                if (bmpUri != null) {
+                    // Construct a ShareIntent with link to image
+                    val shareIntent = Intent()
+                    shareIntent.action = Intent.ACTION_SEND
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri)
+                    shareIntent.type = "image/png"
+                    shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    // Launch sharing dialog for image
+                    startActivity(Intent.createChooser(shareIntent, "Share Image"))
+                } else {
+                    // ...sharing failed, handle error
+                }
+                // setting the visibility of the progressbar to invisible
+                // or you can use View.GONE instead of invisible
+                // View.GONE will remove the progressbar
+                progressBar.visibility = View.INVISIBLE
+            }).start()
         }
-        save.setOnClickListener(){
+        save.setOnClickListener() {
             // get the bitmap of the view using
             // getScreenShotFromView method it is
             // implemented below
             val bitmap = getScreenShotFromView(mPhotograp)
             // if bitmap is not null then
             // save it to gallery
+            var path = ""
             if (bitmap != null) {
-                saveMediaToStorage(bitmap)
+                path = saveMediaToStorage(bitmap)
             }
+            setNotificationChannelIntent(id,imagePath = path)
             finish()
         }
-        mRecyclerList=findViewById(R.id.recyclerList)
-        mHeros= mutableListOf<Hero>()
-        mHeroAdapter= HeroAdapter(this, object: HeroAdapter.OnItemClickListener{
+        mRecyclerList = findViewById(R.id.recyclerList)
+        mHeros = mutableListOf()
+        mHeroAdapter = HeroAdapter(this, object : HeroAdapter.OnItemClickListener {
             override fun onItemClick(item: Hero?) {
-                val uriItem: Uri= Uri.parse(item?.imagePath)
+                val uriItem: Uri = Uri.parse(item?.imagePath)
                 mPhotograp.setImageURI(uriItem)
             }
+
             override fun onOpenFolderClick() {
                 startGalleryForResult()
             }
         })
-        mRecyclerList.adapter=mHeroAdapter
-        mRecyclerList.layoutManager=LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        resultLauncher  = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                // There are no request codes
-                val data: Intent? = result.data
-                val uriGallery: Uri = Uri.parse(data?.data.toString())
-                mPhotograp.setImageURI(uriGallery)
+        mRecyclerList.adapter = mHeroAdapter
+        mRecyclerList.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    // There are no request codes
+                    val data: Intent? = result.data
+                    val uriGallery: Uri = Uri.parse(data?.data.toString())
+                    mPhotograp.setImageURI(uriGallery)
+                }
             }
-        }
         listAllImage()
         mHeroAdapter.setList(mHeros)
+        createNotificationChannel(id)
+    }
+
+    private fun setNotificationChannelIntent(id: String, imagePath: String) {
+        val intent = Intent(this, ImageEditorActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            putExtra("FILE_PATH", imagePath)
+        }
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE)
+
+        val builder = NotificationCompat.Builder(this, id)
+            .setSmallIcon(R.drawable.notification_icon)
+            .setContentTitle("Save file")
+            .setContentText("Finish!!")
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("Finish")
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(1, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel(id: String) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
 
     }
-    fun getLocalBitmapUri(imageView: ImageView): Uri? {
+
+    private fun getLocalBitmapUri(imageView: ImageView): Uri? {
         // Extract Bitmap from ImageView drawable
         val drawable = imageView.drawable
         var bmp: Bitmap? = null
@@ -116,19 +199,52 @@ class ImageEditorActivity : AppCompatActivity() {
             // Use methods on Context to access package-specific directories on external storage.
             // This way, you don't need to request external read/write permission.
             // See https://youtu.be/5xVh-7ywKpE?t=25m25s
-            val file = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png")
-            val out = FileOutputStream(file.absoluteFile,false)
+            val file = File(
+                getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                "share_image_" + System.currentTimeMillis() + ".png"
+            )
+            val out = FileOutputStream(file.absoluteFile, false)
             bmp.compress(Bitmap.CompressFormat.PNG, 100, out)
             out.close()
             // **Warning:** This will fail for API >= 24, use a FileProvider as shown below instead.
-            bmpUri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName().toString() + ".provider", file)
+            bmpUri = FileProvider.getUriForFile(
+                this,
+                this.applicationContext.packageName.toString() + ".provider",
+                file
+            )
         } catch (e: IOException) {
             e.printStackTrace()
         }
         return bmpUri
     }
 
-    // Method when launching drawable within Glide
+    /**
+     *
+     */
+    public class Handler {
+        /**
+         *
+         */
+        @Deprecated("", ReplaceWith("throw RuntimeException(\"Stub!\")"))
+        fun Handler() {
+            throw RuntimeException("Stub!")
+        }
+
+        /**
+         *
+         */
+        @Deprecated("", ReplaceWith("throw RuntimeException(\"Stub!\")"))
+        fun Handler(callback: android.os.Handler.Callback?) {
+            throw RuntimeException("Stub!")
+        }
+
+
+        /**
+         *
+         */
+        fun post(runnable: Runnable) {
+        }
+    }
 
     private fun getScreenShotFromView(v: View): Bitmap? {
         // create a bitmap object
@@ -139,7 +255,8 @@ class ImageEditorActivity : AppCompatActivity() {
             // requires three parameters
             // width and height of the view and
             // the background color
-            screenshot = Bitmap.createBitmap(v.measuredWidth, v.measuredHeight, Bitmap.Config.ARGB_8888)
+            screenshot =
+                Bitmap.createBitmap(v.measuredWidth, v.measuredHeight, Bitmap.Config.ARGB_8888)
             // Now draw this bitmap on a canvas
             val canvas = Canvas(screenshot)
             v.draw(canvas)
@@ -149,11 +266,12 @@ class ImageEditorActivity : AppCompatActivity() {
         // return the bitmap
         return screenshot
     }
+
     // this method saves the image to gallery
-    private fun saveMediaToStorage(bitmap: Bitmap) {
+    private fun saveMediaToStorage(bitmap: Bitmap): String {
         // Generating a file name
         val filename = "${System.currentTimeMillis()}.jpg"
-
+        var imagePath = ""
         // Output stream
         var fos: OutputStream? = null
 
@@ -173,32 +291,42 @@ class ImageEditorActivity : AppCompatActivity() {
 
                 // Inserting the contentValues to
                 // contentResolver and getting the Uri
-                val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
 
                 // Opening an outputstream with the Uri that we got
                 fos = imageUri?.let { resolver.openOutputStream(it) }
+                imagePath= imageUri.toString()
             }
         } else {
             // These for devices running on android < Q
-            val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val imagesDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
             val image = File(imagesDir, filename)
-            fos = FileOutputStream(image)
+            imagePath=image.absolutePath
+            fos = FileOutputStream(image.absoluteFile)
         }
 
         fos?.use {
             // Finally writing the bitmap to the output stream that we opened
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, it)
-            Toast.makeText(this , "Captured View and saved to Gallery" , Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Captured View and saved to Gallery", Toast.LENGTH_SHORT).show()
         }
+        return imagePath
     }
+
+    /**
+     *
+     */
     fun startGalleryForResult() {
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
         resultLauncher.launch(intent)
     }
-    private fun listAllImage(){
-        mHeros.add(Hero("",viewType = HeroAdapter.VIEW_TYPE_TWO))
+
+    private fun listAllImage() {
+        mHeros.add(Hero("", viewType = HeroAdapter.VIEW_TYPE_TWO))
         val isSDPresent = Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
-        if(!isSDPresent) return
+        if (!isSDPresent) return
         val columns = arrayOf(MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID)
         val orderBy = MediaStore.Images.Media._ID
 //Stores all the images from the gallery in Cursor
@@ -207,20 +335,22 @@ class ImageEditorActivity : AppCompatActivity() {
             null, orderBy
         )
 //Total number of images
-        val count: Int = cursor?.count ?:0
+        val count: Int = cursor?.count ?: 0
 
         val arrPath = arrayOfNulls<String>(count)
 
         for (i in 0 until count) {
             cursor?.moveToPosition(i)
-            val dataColumnIndex: Int = cursor?.getColumnIndex(MediaStore.Images.Media.DATA)?:0
+            val dataColumnIndex: Int = cursor?.getColumnIndex(MediaStore.Images.Media.DATA) ?: 0
             //Store the path of the image
             arrPath[i] = cursor?.getString(dataColumnIndex)
-            mHeros.add(Hero(
-                name ="",
-                imagePath = cursor?.getString(dataColumnIndex).orEmpty()
-                ,viewType = HeroAdapter.VIEW_TYPE_ONE
-            ))
+            mHeros.add(
+                Hero(
+                    name = "",
+                    imagePath = cursor?.getString(dataColumnIndex).orEmpty(),
+                    viewType = HeroAdapter.VIEW_TYPE_ONE
+                )
+            )
             Log.i("PATH", arrPath[i].orEmpty())
         }
 // The cursor should be freed up after use with close()
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.Manifest
 import android.app.Activity
+import android.app.PendingIntent
 import android.content.Context
 import android.content.ContextWrapper
 import android.content.Intent
@@ -20,6 +21,8 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.HeroAdapter.Companion.VIEW_TYPE_ONE
@@ -29,71 +32,102 @@ import java.io.File
 import java.io.FileOutputStream
 
 
+/**
+ *
+ */
 class MainActivity : AppCompatActivity() {
-    private lateinit var mHeros:MutableList<Hero>
-    private lateinit var  mRecyclerHero:RecyclerView
+    private lateinit var mHeros: MutableList<Hero>
+    private lateinit var mRecyclerHero: RecyclerView
     private lateinit var mHeroAdapter: HeroAdapter
-    lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
+    /**
+     *
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mRecyclerHero=findViewById(R.id.recyclerHero)
-        mHeros= mutableListOf<Hero>()
+        mRecyclerHero = findViewById(R.id.recyclerHero)
+        mHeros = mutableListOf<Hero>()
         val gridLayoutManager = GridLayoutManager(applicationContext, 3)
-        mHeroAdapter= HeroAdapter(this, object: HeroAdapter.OnItemClickListener{
+        mHeroAdapter = HeroAdapter(this, object : HeroAdapter.OnItemClickListener {
             override fun onItemClick(item: Hero?) {
                 item?.imagePath?.let { startImageEditor(imagePath = it) }
             }
+
             override fun onOpenFolderClick() {
                 startGalleryForResult()
             }
         })
-        mRecyclerHero.adapter=mHeroAdapter
+        mRecyclerHero.adapter = mHeroAdapter
         //mRecyclerHero.layoutManager=LinearLayoutManager(this)
-        mRecyclerHero.layoutManager=gridLayoutManager
+        mRecyclerHero.layoutManager = gridLayoutManager
         askPermission()
-        val mFloatingActionButton: FloatingActionButton= findViewById(R.id.camera)
-        mFloatingActionButton.setOnClickListener(){
+        val mFloatingActionButton: FloatingActionButton = findViewById(R.id.camera)
+        mFloatingActionButton.setOnClickListener() {
 
             takePicture.launch(null)
         }
-        resultLauncher  = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                // There are no request codes
-                val data: Intent? = result.data
-                val myIntent = Intent(this, ImageEditorActivity::class.java)
-                myIntent.putExtra("FILE_PATH",data?.data.toString())
-                this.startActivity(myIntent)
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    // There are no request codes
+                    val data: Intent? = result.data
+                    val myIntent = Intent(this, ImageEditorActivity::class.java)
+                    myIntent.putExtra("FILE_PATH", data?.data.toString())
+                    this.startActivity(myIntent)
+                }
             }
-        }
     }
+
+    /**
+     *
+     */
     fun startGalleryForResult() {
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
         resultLauncher.launch(intent)
     }
-    private fun startImageEditor(imagePath: String){
+
+    private fun startImageEditor(imagePath: String) {
         val myIntent = Intent(this, ImageEditorActivity::class.java)
-        myIntent.putExtra("FILE_PATH",imagePath)
+        myIntent.putExtra("FILE_PATH", imagePath)
         this.startActivity(myIntent)
     }
 
-    private fun askPermission(){
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
-        }
-        else {
+    private fun askPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                1
+            )
+        } else {
             listAllImage()
             mHeroAdapter.setList(mHeros)
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    /**
+     *
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             1 -> {
                 if ((grantResults.isNotEmpty() &&
-                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                ) {
                     listAllImage()
                     mHeroAdapter.setList(mHeros)
                 } else {
@@ -105,10 +139,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-    private fun listAllImage(){
-        mHeros.add(Hero("",viewType = VIEW_TYPE_TWO))
+
+
+
+    private fun listAllImage() {
+        mHeros.add(Hero("", viewType = VIEW_TYPE_TWO))
         val isSDPresent = Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
-        if(!isSDPresent) return
+        if (!isSDPresent) return
         val columns = arrayOf(MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID)
         val orderBy = MediaStore.Images.Media._ID
 //Stores all the images from the gallery in Cursor
@@ -117,46 +154,51 @@ class MainActivity : AppCompatActivity() {
             null, orderBy
         )
 //Total number of images
-        val count: Int = cursor?.count ?:0
+        val count: Int = cursor?.count ?: 0
 
         val arrPath = arrayOfNulls<String>(count)
 
         for (i in 0 until count) {
             cursor?.moveToPosition(i)
-            val dataColumnIndex: Int = cursor?.getColumnIndex(MediaStore.Images.Media.DATA)?:0
+            val dataColumnIndex: Int = cursor?.getColumnIndex(MediaStore.Images.Media.DATA) ?: 0
             //Store the path of the image
             arrPath[i] = cursor?.getString(dataColumnIndex)
-            mHeros.add(Hero(
-                name ="image $i",
-                imagePath = cursor?.getString(dataColumnIndex).orEmpty()
-            ,viewType = VIEW_TYPE_ONE))
+            mHeros.add(
+                Hero(
+                    name = "image $i",
+                    imagePath = cursor?.getString(dataColumnIndex).orEmpty(),
+                    viewType = VIEW_TYPE_ONE
+                )
+            )
             Log.i("PATH", arrPath[i].orEmpty())
         }
 // The cursor should be freed up after use with close()
         cursor?.close()
     }
-    private fun creatHeroList()
-    {
+
+    private fun creatHeroList() {
         //mHeros.add(Hero("thor", R.drawable.thor,))
         //mHeros.add(Hero("ironmen", R.drawable.dafodil))
         //mHeros.add(Hero("hulk", R.drawable.hulk))
         //mHeros.add(Hero("spidermen", R.drawable.spiderman))
         //mHeros.add(Hero("sad", com.google.android.material.R.drawable.notification_template_icon_low_bg))
     }
+
     private val takePicture =
         registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
             saveFile(bitmap)
         }
-    private fun saveFile(bitmap: Bitmap){
-        val contextWrapper  =  ContextWrapper(applicationContext)
+
+    private fun saveFile(bitmap: Bitmap) {
+        val contextWrapper = ContextWrapper(applicationContext)
         val directory = contextWrapper.getDir(filesDir.name, Context.MODE_PRIVATE)
-        val file = File(directory,"fileName")
+        val file = File(directory, "fileName")
         val fos = FileOutputStream(file.absolutePath, false) // save
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
-        Log.d("filepath","filepath ${file.absolutePath} length ${file.length()}")
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+        Log.d("filepath", "filepath ${file.absolutePath} length ${file.length()}")
         fos.close()
         val myIntent = Intent(this, ImageEditorActivity::class.java)
-        myIntent.putExtra("FILE_PATH",file.absolutePath)
+        myIntent.putExtra("FILE_PATH", file.absolutePath)
         this.startActivity(myIntent)
     }
 }
