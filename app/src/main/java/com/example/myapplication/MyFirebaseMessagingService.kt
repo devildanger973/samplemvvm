0c2af349b1fc0d5895976cea1294e329c5e23f8d
DESKTOP-B6N5TV2\nhan <devildanger976@gmail.com>
2022-03-29 15:08:48 +0700

    sevice MyFirebaseMessagingService

--- app/src/main/java/com/example/myapplication/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/myapplication/MyFirebaseMessagingService.kt
@@ -2,7 +2,9 @@ package com.example.myapplication
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.util.Log
 import android.widget.Toast
@@ -21,7 +23,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         remoteMessage.notification?.let {
             createNotificationChannel(it.title.toString())
             Log.d("TAG", "remoteMessage ${it.body} ${it.title}")
-            setNotificationChannelIntent(it.title.toString())
+            setNotificationChannelIntent(it.title.toString(), it.body.toString())
         }
 
     }
@@ -34,17 +36,23 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // FCM registration token to your app server.
     }
 
-    private fun setNotificationChannelIntent(id: String) {
-        val builder = NotificationCompat.Builder(this, id)
+    private fun setNotificationChannelIntent(title: String, body: String) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val builder = NotificationCompat.Builder(this, title)
             .setSmallIcon(R.drawable.notification_icon)
-            .setContentTitle(id)
-            .setContentText(id)
+            .setContentTitle(title)
+            .setContentText(body)
             .setStyle(
                 NotificationCompat.BigTextStyle()
-                    .bigText(id)
+                    .bigText(body)
             )
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
             .setAutoCancel(true)
         with(NotificationManagerCompat.from(this)) {
             // notificationId is a unique int for each notification that you must define
@@ -63,18 +71,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             val notificationManager: NotificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
-            val builder = NotificationCompat.Builder(this, id)
-                .setSmallIcon(R.drawable.notification_icon)
-                .setContentTitle(id)
-                .setContentText(id)
-                .setStyle(
-                    NotificationCompat.BigTextStyle()
-                        .bigText(id)
-                )
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                // Set the intent that will fire when the user taps the notification
-                .setAutoCancel(true)
-            notificationManager.notify(2, builder.build())
+
         }
     }
 }
